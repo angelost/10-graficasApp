5250a5ee49d40b825ccf6aa2c0e478c5413b8647
Angelo <angelo.gonzalez@live.com>
2022-09-22 22:19:17 -0300

    338. Gráfica de Dona

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,6 +8,28 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class DonaComponent implements OnInit {
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales', 'Other'];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      {
+        data: [ 350, 450, 100, 150],
+        backgroundColor: ['#094FED','#0A91F7', '#15BDE0', '#00C6F5']
+      }
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 
   constructor() { }
 
