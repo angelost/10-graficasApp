78cd724fd789deb84573924c8bd576c05b17e3f8
Angelo <angelo.gonzalez@live.com>
2022-09-22 21:24:17 -0300

    335. Personalizando la gráfica

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -36,10 +36,11 @@ export class BarrasComponent {
   public barChartPlugins = [DataLabelsPlugin];
  
   public barChartData: ChartData<'bar'> = {
-    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#F64F68', hoverBackgroundColor: 'red'},
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#D444A2', hoverBackgroundColor: 'red'},
+      { data: [82, 33, 25, 56, 34, 87, 67], label: 'Series C', backgroundColor: '#DF56EB', hoverBackgroundColor: 'red'}
     ],
   };
  
@@ -65,15 +66,35 @@ export class BarrasComponent {
   }
  
   public randomize(): void {
-    // Only Change 3 values
+
     this.barChartData.datasets[0].data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
       Math.round(Math.random() * 100),
-      56,
       Math.round(Math.random() * 100),
-      40,
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)      
+    ];
+
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)      
+    ];
+
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)      
     ];
  
     this.chart?.update();
