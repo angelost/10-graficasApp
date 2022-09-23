4d43b6922b09c68b58f7368f5b8619d6ef092f69
Angelo <angelo.gonzalez@live.com>
2022-09-22 22:03:02 -0300

    337. Añadir flexibilidad

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild, OnInit } from '@angular/core';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 import { BaseChartDirective } from 'ng2-charts';
@@ -13,35 +13,27 @@ export class GraficaBarraComponent {
 
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
  
-  public barChartOptions: ChartConfiguration['options'] = {
+  @Input() barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
     plugins: {
       legend: {
         display: true,
-      },
-      datalabels: {
-        anchor: 'end',
-        align: 'end',
-      },
-    },
+      }
+    }
   };
-  public barChartType: ChartType = 'bar';
-  public barChartPlugins = [DataLabelsPlugin];
  
-  public barChartData: ChartData<'bar'> = {
-    labels: ['2020', '2021', '2022', '2023', '2024', '2025', '2026'],
+  @Input() barChartType: ChartType = 'bar';
+ 
+  
+ 
+  @Input() barChartData: ChartData = {
+    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
     datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A', backgroundColor: '#F64F68', hoverBackgroundColor: 'red'},
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', backgroundColor: '#D444A2', hoverBackgroundColor: 'red'},
-      { data: [82, 33, 25, 56, 34, 87, 67], label: 'Series C', backgroundColor: '#DF56EB', hoverBackgroundColor: 'red'}
-    ],
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' },
+      { data: [ 82, 28, 1, 39, 46, 67, 50 ], label: 'Series C' }
+      
+    ]
   };
 
 }
